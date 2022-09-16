f96826b7c7d806b4892e2078323a88d0611ed2c6
sarpatalik <fuhreredits@gmail.com>
2022-09-16 21:27:17 +0300

    Update of kokal, kareal, and the proof of my webpg

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // Operatör al (+, -, /, *).
-var op = prompt("Islem Seciniz (+, -, /, *)");
 
-// Sayilari al.
-var sayiBir = prompt("Birinci Sayiyi Giriniz:");
-var sayiIki = prompt("Ikinci Sayiyi Giriniz:");
+var op = prompt("Islem Seciniz (+, -, /, *, kokal, kareal)");
 
-// İf statement oluştur.
+
+// If statement oluştur.
+
 if(op == "+"){
+    var sayiBir = prompt("Birinci Sayiyi Giriniz:");
+    var sayiIki = prompt("Ikinci Sayiyi Giriniz:");
     var result = parseFloat(sayiBir) + parseFloat(sayiIki);
 } else if(op == "-") {
+    var sayiBir = prompt("Birinci Sayiyi Giriniz:");
+    var sayiIki = prompt("Ikinci Sayiyi Giriniz:");
     var result = parseFloat(sayiBir) - parseFloat(sayiIki);
 } else if(op == "/") {
+    var sayiBir = prompt("Birinci Sayiyi Giriniz:");
+    var sayiIki = prompt("Ikinci Sayiyi Giriniz:");
     var result = parseFloat(sayiBir) / parseFloat(sayiIki);
 } else if(op == "*") {
+    var sayiBir = prompt("Birinci Sayiyi Giriniz:");
+    var sayiIki = prompt("Ikinci Sayiyi Giriniz:");
     var result = parseFloat(sayiBir) * parseFloat(sayiIki);
 } else if (op == "tirbuşon"){
     console.log("Beklenmeyen Operatör");
     var result = "ALLAH";
-} else {
+} else if (op == "kokal"){
+    var sayiBir = prompt("Birinci Sayiyi Giriniz:");
+    var result = Math.sqrt(sayiBir);
+} else if (op =="kareal"){
+    var sayiBir = prompt("Birinci Sayiyi Giriniz:");
+    var result = parseFloat(sayiBir) * parseFloat(sayiBir);
+    
+}else {
     console.log("Beklenmeyen Operatör Girişi");
     var result = "undefined";
 }
@@ -25,3 +39,4 @@ if(op == "+"){
 // Sonucu yazdır.
 
 document.getElementById("res").innerHTML = result;
+// Sarp 9-G.
